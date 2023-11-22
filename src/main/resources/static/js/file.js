bc1097a6f3b272898833340b634c96c9c7324fa6
wangkai <2103623596@qq.com>
2023-11-22 23:28:34 +0800

    文件发送进度条

--- src/main/resources/static/js/file.js
+++ src/main/resources/static/js/file.js
@@ -1,4 +1,5 @@
 var inputfile;
+var processBar= $("#progressBar");
 function sendFileClick() {
 
     if(!currentUserInfo.to){
@@ -29,17 +30,114 @@ function uploadFiles() {
     msg.to = currentUserInfo.to;
     formData.set("msg",JSON.stringify(msg));
 
-    $.ajax({
-        url: '/uploadFiles',
-        type: 'POST',
-        cache: false,
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: successCallback,
-        error: errorCallback,
+    var xhr = new XMLHttpRequest();
+    //true为异步处理
+    xhr.open('post','/uploadFiles', true);
+    //上传开始执行方法
+    xhr.onloadstart = function() {
+        console.log('开始上传')
+        ot = new Date().getTime();   //设置上传开始时间
+        oloaded = 0;//已上传的文件大小为0
+    };
+
+    xhr.upload.addEventListener('progress', progressFunction, false);
+    xhr.addEventListener("load", uploadComplete, false);
+    xhr.addEventListener("error", uploadFailed, false);
+    xhr.addEventListener("abort", uploadCanceled, false);
+    xhr.send(); // 发送ajax请求
+
+    // $.ajax({
+    //     url: '/uploadFiles',
+    //     type: 'POST',
+    //     cache: false,
+    //     data: formData,
+    //     processData: false,
+    //     contentType: false,
+    //     success: successCallback,
+    //     error: errorCallback,
+    // })
+
+
+    function progressFunction(evt) {
+        debugger;
+        if (evt.lengthComputable) {
+            var completePercent = Math.round(evt.loaded / evt.total * 100)
+                + '%';
+            processBar.width(completePercent);
+            processBar.text(completePercent);
+
+            var time = $("#time");
+            var nt = new Date().getTime();     //获取当前时间
+            var pertime = (nt-ot)/1000;        //计算出上次调用该方法时到现在的时间差，单位为s
+            ot = new Date().getTime();          //重新赋值时间，用于下次计算
+
+            var perload = evt.loaded - oloaded; //计算该分段上传的文件大小，单位b
+            oloaded = evt.loaded;               //重新赋值已上传文件大小
+
+            //上传速度计算
+            var speed = perload/pertime;//单位b/s
+            var bspeed = speed;
+            var units = 'b/s';//单位名称
+            if(speed/1024>1){
+                speed = speed/1024;
+                units = 'k/s';
+            }
+            if(speed/1024>1){
+                speed = speed/1024;
+                units = 'M/s';
+            }
+            speed = speed.toFixed(1);
+            //剩余时间
+            var resttime = ((evt.total-evt.loaded)/bspeed).toFixed(1);
+            $("#showInfo").html(speed+units+'，剩余时间：'+resttime+'s');
+        }
+    }
+
+    //上传成功后回调
+    function uploadComplete(evt) {
+        //uploadBtn.attr('disabled', false);
+        console.log('上传完成')
+    };
+
+    //上传失败回调
+    function uploadFailed(evt) {
+        console.log('上传失败' + evt.target.responseText);
+    }
+
+    //终止上传
+    function cancelUpload() {
+        xhr.abort();
+    }
+
+    //上传取消后回调
+    function uploadCanceled(evt) {
+        console.log('上传取消,上传被用户取消或者浏览器断开连接:' + evt.target.responseText);
+    }
+
+    canelBtn.click(function(){
+        //uploadBtn.attr('disabled', false);
+        cancelUpload();
     })
 }
+function getSize(size) {
+    var fileSize = '0KB';
+    if (size > 1024 * 1024) {
+        fileSize = (Math.round(size / (1024 * 1024))).toString() + 'MB';
+    } else {
+        fileSize = (Math.round(size / 1024)).toString() + 'KB';
+    }
+    return fileSize;
+}
+function setProgress(w) {
+    processBar.width(w + '%');
+}
+function showProgress() {
+    processBar.parent().show();
+}
+function hideProgress() {
+    processBar.parent().hide();
+}
+
 
 function successCallback(data) {
     console.log(data)
@@ -74,7 +172,6 @@ function uploadfile(filename) {
     xhr.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
     // xhr.setRequestHeader('Authorization', `Bearer ${JSON.parse(Cookies.get('tm_token'))}`);
     xhr.responseType = "blob"; // 返回类型blob
-    xhr.send(); // 发送ajax请求
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             //创建blob对象
